e34b2594038c46db89d8879802a98506925a74b1
andrija-s <51809572+andrija-s@users.noreply.github.com>
2021-11-05 15:42:35 -0700

    stuff

--- assets/scripts/Router.js
+++ assets/scripts/Router.js
@@ -38,6 +38,7 @@ export class Router {
      * router instance using the 'this' keyword. Substitute 'home' for the variable
      * page
      */
+    this[page] = pageFunc;
   }
 
   /**
@@ -65,5 +66,14 @@ export class Router {
      *     and URL + hash to history
      *  4. Finally, call the stored function for the given page
      */
+    if (!this[page]) {
+       console.log(error);
+       return;
+    }
+    let hash = (page == 'home') ? '' : '#' + page;
+    if (!statePopped && window.location.hash != hash) {
+       history.pushState(history.state, '', window.location + hash);
+    }
+    this[page];
   }
 }
